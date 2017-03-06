98801c7882cc0a0ddfabea8254153aea22a1c417
hanishar <hanisha946@gmail.com>
2017-03-06 17:58:28 +0530

    Hanisha | Start xvfb process for running tests

--- ui/scripts/package.sh
+++ ui/scripts/package.sh
@@ -13,6 +13,16 @@ grunt bundle
 mkdir chromeDist
 mkdir androidDist
 
+if [ $(pgrep Xvfb) ]; then
+    XVFB_PID=$(pgrep Xvfb)
+    echo "Killing Xvfb process $XVFB_PID"
+    kill $XVFB_PID
+fi
+export DISPLAY=:99
+Xvfb :99 &
+XVFB_PID=$!
+echo "Starting Xvfb process $XVFB_PID"
+
 rm -rf dist/*
 
 grunt chrome
@@ -23,6 +33,9 @@ rm -rf dist/*
 
 grunt android
 cp -r dist/* androidDist
+
+echo "Killing Xvfb process $XVFB_PID"
+/usr/bin/sudo kill $XVFB_PID
 
 
 
