ca1579e253b96fb920ecdda789ad4a3843d27719
hanishar <hanisha946@gmail.com>
2017-03-06 12:07:23 +0530

    Hanisha | #3307 | Removed grunt tasks needed for online app from package.sh

--- ui/scripts/package.sh
+++ ui/scripts/package.sh
@@ -3,48 +3,24 @@
 export LANG=en_US.UTF-8
 set -e
 
-BASE_DIR=`dirname $0`
-ROOT_DIR=$BASE_DIR/..
-ZIP_FILE_NAME=bahmniapps
+npm cache clean
+bower cache clean
 
-mkdir -p $ROOT_DIR/target
-rm -rf $ROOT_DIR/target/${ZIP_FILE_NAME}*.zip
+mkdir chromeDist
+mkdir androidDist
 
-npm install
-bower install
-grunt bundle
-grunt uglify-and-rename
+rm -rf dist/*
 
-cd $ROOT_DIR
-
-rm -rf cacheChromeDist cacheAndroidDist
-cp -r dist cacheChromeDist
-cp -r dist cacheAndroidDist
-
-if [ $(pgrep Xvfb) ]; then
-    XVFB_PID=$(pgrep Xvfb)
-    echo "Killing Xvfb process $XVFB_PID"
-    kill $XVFB_PID
-fi
-export DISPLAY=:99
-Xvfb :99 &
-XVFB_PID=$!
-echo "Starting Xvfb process $XVFB_PID"
-
-grunt web
-cd dist && zip -r ../target/${ZIP_FILE_NAME}.zip *
-
-cd ..
-rm -rf dist
-cp -r cacheChromeDist dist
 grunt chrome
-cd dist && zip -r ../target/${ZIP_FILE_NAME}_chrome.zip *
+cp -r dist/* chromeDist
+zip -r bahmni-connect-apps.zip chromeDist/*
 
-cd ..
-rm -rf dist
-cp -r cacheAndroidDist dist
+rm -rf dist/*
+
 grunt android
-cd dist && zip -r ../target/${ZIP_FILE_NAME}_android.zip *
+cp -r dist/* androidDist
 
-echo "Killing Xvfb process $XVFB_PID"
-/usr/bin/sudo kill $XVFB_PID
+
+
+
+
