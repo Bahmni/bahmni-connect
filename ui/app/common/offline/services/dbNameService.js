07c76936c3565e21b57bb323ef72fa98fbb64bac
sumanmaity112 <suman.maity112@gmail.com>
2017-03-13 21:43:31 +0530

    Suman | #3010 | Show error message when dbNameCondition is not present in config

--- ui/app/common/offline/services/dbNameService.js
+++ ui/app/common/offline/services/dbNameService.js
@@ -1,33 +1,38 @@
 'use strict';
 
 angular.module('bahmni.common.offline')
-    .service('dbNameService', ["offlineService", "offlineDbService", "androidDbService", "$q", function (offlineService, offlineDbService, androidDbService, $q) {
-        var loadDbNameService = function () {
-            var isOfflineApp = offlineService.isOfflineApp();
-            if (isOfflineApp) {
-                var defer = $q.defer();
-                if (offlineService.isAndroidApp()) {
-                    offlineDbService = androidDbService;
+    .service('dbNameService', ["offlineService", "offlineDbService", "androidDbService", "$q", "messagingService",
+        function (offlineService, offlineDbService, androidDbService, $q, messagingService) {
+            var loadDbNameService = function () {
+                var isOfflineApp = offlineService.isOfflineApp();
+                if (isOfflineApp) {
+                    var defer = $q.defer();
+                    if (offlineService.isAndroidApp()) {
+                        offlineDbService = androidDbService;
+                    }
+                    offlineDbService.getConfig("dbNameCondition").then(function (config) {
+                        if (!config || !config.value) {
+                            messagingService.showMessage("error", "dbNameCondition.json is not present in config");
+                            return defer.reject();
+                        }
+                        var script = config.value['dbNameCondition.js'];
+                        eval(script); // eslint-disable-line no-eval
+                        return defer.resolve();
+                    });
                 }
-                offlineDbService.getConfig("dbNameCondition").then(function (config) {
-                    var script = config.value['dbNameCondition.js'];
-                    eval(script); // eslint-disable-line no-eval
-                    defer.resolve();
+                return defer.promise;
+            };
+
+            var getDbName = function (provider, loginLocation) {
+                if (!offlineService.getItem("allowMultipleLoginLocation")) {
+                    return $q.when("Bahmni Connect");
+                }
+                return loadDbNameService().then(function () {
+                    return Bahmni.Common.Offline.dbNameCondition.get(provider, loginLocation);
                 });
-            }
-            return defer.promise;
-        };
+            };
 
-        var getDbName = function (provider, loginLocation) {
-            if (!offlineService.getItem("allowMultipleLoginLocation")) {
-                return $q.when("Bahmni Connect");
-            }
-            return loadDbNameService().then(function () {
-                return Bahmni.Common.Offline.dbNameCondition.get(provider, loginLocation);
-            });
-        };
-
-        return {
-            getDbName: getDbName
-        };
-    }]);
+            return {
+                getDbName: getDbName
+            };
+        }]);
