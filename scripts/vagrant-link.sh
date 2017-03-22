bdc4fd2f28afd05178870075e8502fca7ed3a7d6
sumanmaity112 <suman.maity112@gmail.com>
2017-03-22 09:41:52 +0530

    Suman | Fix vagrant-link script

--- scripts/vagrant-link.sh
+++ scripts/vagrant-link.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -x -e
 
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+source ${PATH_OF_CURRENT_SCRIPT}/vagrant_functions.sh
 #USER=jss
 USER=bahmni
 
 if [ "$#" == "0" ]; then
-	FOLDER="app"
+	FOLDER="dist"
 else
 	FOLDER="$1"
 fi
 
-run_in_vagrant -c "sudo rm -rf /var/www/bahmniapps"
-run_in_vagrant -c "sudo ln -s /bahmni/openmrs-module-bahmniapps/ui/$FOLDER/ /var/www/bahmniapps"
-run_in_vagrant -c "sudo chown -h ${USER}:${USER} /var/www/bahmniapps"
+run_in_vagrant -c "sudo rm -rf /var/www/bahmni-connect-apps"
+run_in_vagrant -c "sudo ln -s /bahmni/bahmni-connect/ui/$FOLDER/ /var/www/bahmni-connect-apps"
+run_in_vagrant -c "sudo chown -h ${USER}:${USER} /var/www/bahmni-connect-apps"
 
 run_in_vagrant -c "sudo rm -rf /var/www/style-guide"
 run_in_vagrant -c "sudo ln -s /bahmni/openmrs-module-bahmniapps/ui/style-guide/ /var/www/style-guide"
